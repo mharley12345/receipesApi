085d0494a4d40162721181777930a0afa92fd45c
Mike Harley <mharley12345@gmail.com>
2020-01-16 21:40:18 -0800

    .

--- express/server.js
+++ express/server.js
@@ -13,7 +13,7 @@ router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('./functions/server', router);  // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
